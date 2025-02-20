738d88aec2b7f47c6ab2f220f33e799519d9b506
Jose Roberto <joserobertomi@outlook.com>
2025-02-20 02:01:08 -0300

    fix: requests fk patients

--- api_requests.py
+++ api_requests.py
@@ -69,6 +69,6 @@ def get_appointments_df(api_url: str) -> pl.DataFrame:
         date_cols=["scheduling_date", "appointment_date"],
         time_cols=["appointment_time", "check_in_time", "start_time", "end_time"],
         timedelta_cols = ["appointment_duration", "waiting_time"],
-        int_cols=["appointment_id", "patient_id", "appointment_id"],
-        drop_cols=["id", "patient_id"]
+        int_cols=["appointment_id", "patient_id"],
+        drop_cols=["id"]
     )
